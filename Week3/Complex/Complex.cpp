e23a0045cd1eae1d65ec9aa471456db58b0fe477
Dylan Kinzer <dylankinzer@Dylans-MacBook-Pro.local>
2019-03-26 21:23:54 -0700

    Changes to complex class

--- Week3/Complex/Complex.cpp
+++ Week3/Complex/Complex.cpp
@@ -1,12 +1,96 @@
 #include "Complex.h"
 
 
-
-Complex::Complex()
+namespace CSC252HW3
 {
+    Complex::Complex() : m_real(0), m_imag(0) {}
+    
+    Complex::Complex(double real, double imaginary) : m_real(real), m_imag(imaginary)
+    {
+        
+    }
+    
+    
+    //MUTATORS
+    void Complex::setReal(double myRealNumber)
+    {
+        this->m_real = myRealNumber;
+    }
+    
+    void Complex::setImag(double imagNum)
+    {
+        this->m_imag = imagNum;
+    }
+    
+    //ACCESSORS
+    const double Complex::getReal()
+    {
+        return m_real;
+    }
+    
+    const double Complex::getImag()
+    {
+        return m_imag;
+    }
+    
+    // Add
+    Complex Complex::operator +(const Complex& complexNum)
+    {
+        Complex  sum;
+        sum.m_imag = this->m_imag + complexNum.m_imag;
+        sum.m_real = this->m_real + complexNum.m_real;
+        
+        return sum;
+    }
+    
+    // Subtract
+    Complex Complex::operator -(const Complex& complexNum)
+    {
+        return complexNum;
+    }
+    
+    // Multiply
+    Complex Complex::operator *(const Complex& complexNum)
+    {
+     return complexNum;
+    }
+    
+    // Divide
+    Complex Complex::operator /(const Complex& complexNum)
+    {
+        return complexNum;
+    }
+    
+    // set equal to
+    Complex& Complex::operator =(const Complex& complexNum)
+    {
+        this->m_real = complexNum.m_real;
+        this->m_imag = complexNum.m_imag;
+        return *this;
+    }
+    /*
+    // compare
+    Complex operator !=(const Complex& complexNum)
+    {
+        
+    }
+    
+    // print out
+    Complex operator <<(const Complex& complexNum)
+    {
+        
+    }
+    
+    // read in
+    Complex operator >>(const Complex& complexNum)
+    {
+        
+    }
+    */
+    //DTOR
+    Complex::~Complex()
+    {
+    }
+
 }
 
-
-Complex::~Complex()
-{
-}
--- Week3/Complex/Complex.h
+++ Week3/Complex/Complex.h
@@ -1,8 +1,56 @@
 #pragma once
-class Complex
+#include <string>
+namespace CSC252HW3
 {
-public:
-	Complex();
-	~Complex();
-};
+    using std::string;
+    class Complex
+    {
+    public:
+        
+        // CTOR
+        Complex(double, double);
+        
+        // MUTATORS
+        void setReal(double);
+        void setImag(double);
+        
+        // ACCESSORS
+        const double getReal();
+        const double getImag();
+        
+        // Add
+        Complex operator +(const Complex& complexNum); // const?
+        
+        // Subtract
+        Complex operator -(const Complex& complexNum); // const?
+        
+        // Multiply
+        Complex operator *(const Complex& complexNum); // const?
+        
+        // Divide
+        Complex operator /(const Complex& complexNum); // const?
+        
+        
+        // set equal to
+        Complex& operator =(const Complex& complexNum); //const?
+        /*
+        // compare
+        Complex operator !=(const Complex& complexNum) const;
+        
+        // print out
+        Complex operator <<(const Complex& complexNum) const;
+        
+        // read in
+        Complex operator >>(const Complex& complexNum) const;
+        
+         */
+        // DTOR
+        ~Complex();
+        
+    private:
+        double m_real;
+        double m_imag;
+        Complex();
+    };
 
+}

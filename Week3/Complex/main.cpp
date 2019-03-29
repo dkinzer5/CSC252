c828b5b4e639af86e967f6a6d6f4f788762ec0f6
dkinzer5 <d.kinzer5@gmail.com>
2019-03-29 11:19:41 -0700

    added main for complex class

--- Week3/Complex/main.cpp
+++ Week3/Complex/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Complex.h"
+
 using std::cout;
 using std::cin;
 using std::endl;
@@ -11,27 +12,29 @@ int main()
 	double dReal1 = 5.0;
 	double dImage1 = 5.0;
 
-	double dReal2 = 5.0;
-	double dImage2 = 5.1;
+	double dReal2 = 13.73;
+	double dImage2 = 4.65;
 
 	double dReal3 = 7.0;
 	double dImage3 = 7.0;
 
 	Complex comp1(dReal1, dImage1);
-	Complex comp2(dReal2, dImage2);
+	Complex comp2(dReal1, dImage1);
 	Complex comp3(dReal3, dImage3);
+	Complex comp4(dReal2, dReal3);
 
-	// TESTS add and assignment
+	// TESTS add and assignment - should be 10 + 10i
 	comp3 = comp2 + comp1;
-	cout << comp3.getReal() << endl << comp3.getImag() << endl;
+	cout << "Complex number 3 is: " << comp3 << endl;
 
 	// TESTS minus and assigment
-	comp3 = comp2 - comp1;
-	cout << comp3.getReal() << endl << comp3.getImag() << endl;
+	comp4 = comp3 - comp1;
+	cout << "Complex number 4 is: " << comp4 << endl;
 
 	// TESTS multiply operator
 	comp3 = comp1 * comp2;
-	cout << comp3.getReal() << endl << comp3.getImag() << endl;
+	cout << comp3 << endl;
+
 
 	// Tests not equal comparison
 	if (comp1 != comp2)
@@ -43,8 +46,10 @@ int main()
 		cout << "They must be equal!\n";
 	}
 
-	cout << comp1 << endl;
+	cout << "Enter the real portion and imaginary portion (separated by a space) : ";
+	cin >> comp4;
 
+	cout << "Your complex number is: " << comp4 << endl;
 	return 0;
 
 }
